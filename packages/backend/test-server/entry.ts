43bf2e81acbc0d4350a006afc80a0a61aca99a41
samunohito <46447427+samunohito@users.noreply.github.com>
2024-01-07 17:36:34 +0900

    refactor

--- packages/backend/test-server/entry.ts
+++ packages/backend/test-server/entry.ts
@@ -6,16 +6,9 @@ import { ServerService } from '@/server/ServerService.js';
 import { loadConfig } from '@/config.js';
 
 async function launch() {
-	// 既に重複したポートがある場合はkill
-	const config = loadConfig();
-	try {
-		const pid = await portToPid(config.port);
-		if (pid) {
-			await fkill(pid, { force: true });
-		}
-	} catch {
-		// NOP;
-	}
+	process.env.NODE_ENV = 'test';
+
+	await killTestServer();
 
 	console.log('starting application...');
 
@@ -29,7 +22,22 @@ async function launch() {
 	const serverService = app.get(ServerService);
 	await serverService.launch();
 
+	// ジョブキューは必要な時にテストコード側で起動する
+
 	console.log('application initialized.');
+}
+
+async function killTestServer() {
+	// 既に重複したポートがある場合はkill
+	const config = loadConfig();
+	try {
+		const pid = await portToPid(config.port);
+		if (pid) {
+			await fkill(pid, { force: true });
+		}
+	} catch {
+		// NOP;
+	}
 }
 
 export default launch;
